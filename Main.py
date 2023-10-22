c463103090755b407b9290a8a2e49d813697f928
haninafisazka <haninafisazka@gmail.com>
2023-10-22 22:19:59 +0700

    update main.py and math_function.py
    make function for multiplication and division

--- Main.py
+++ Main.py
@@ -1,15 +1,26 @@
-from Math_function import add
+from Math_function import add, multiply, divide
 
 
 def main():
 
     data_1 = int(input("masukkan input 1 :"))
     data_2 = int(input("masukkan input 2 :"))
-    operator = input("masukkan operator :")
+    operator = input("masukkan operator (+, *, /): ")
 
     if operator == "+":
         result = add(data_1, data_2)
-
+    elif operator == "*":  # Check for multiplication
+        result = multiply(data_1, data_2)
+    elif operator == "/":  # Check for division
+        try:
+            result = divide(data_1, data_2)
+        except ValueError as e:
+            print("Error:", e)
+            return  # Exit the program in case of division by zero
+    else:
+        print("Operator not supported")
+        return  # Exit the program for unsupported operators
+    
     print("{} {} {} = {} ".format(data_1, operator, data_2, result))
 
 
